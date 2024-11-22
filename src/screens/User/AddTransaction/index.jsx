0ef280d051e02b81e6ef4e418fe2fd0e1a1fe416
rfbittar <rafabittarg@gmail.com>
2024-11-22 20:55:04 -0300

    feat: dropdown das categorias

--- src/screens/User/AddTransaction/index.jsx
+++ src/screens/User/AddTransaction/index.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
-import { Text, TextInput, Button, SegmentedButtons } from 'react-native-paper';
+import { Text, TextInput, Button, SegmentedButtons} from 'react-native-paper';
+import {DropDown} from 'react-native-paper-dropdown'; 
 import { colors } from '../../../theme';
 import SafeScreen from '../../../components/SafeScreen';
+import {useFinances} from '../../../hooks/useFinances'
 
-const AddTransaction = () => {
-  const [transactionType, setTransactionType] = useState('expense');
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState('');
-  const [category, setCategory] = useState('');
-  const [date, setDate] = useState(new Date());
+
+  
+
+  const AddTransaction = () => {
+    const [transactionType, setTransactionType] = useState('expense');
+    const [amount, setAmount] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [date, setDate] = useState(new Date());
+    const { loading , error , categories} = useFinances()
+    console.log('Categorias carregadas:', categories);
+    const [showDropDown, setShowDropDown] = useState(false);
+
+    const formattedCategories = categories?.map((category) => ({
+      label: category.name, // Nome da categoria para exibição
+      value: category.id,   // ID da categoria como valor
+    })) || [];
+
 
   return (
     <SafeScreen>
@@ -45,13 +59,16 @@ const AddTransaction = () => {
             style={styles.input}
           />
 
-          <TextInput
+          <DropDown
             label="Categoria"
-            value={category}
-            onChangeText={setCategory}
             mode="outlined"
+            visible={showDropDown}
+            showDropDown={() => setShowDropDown(true)}
+            onDismiss={() => setShowDropDown(false)}
+            value={category} 
+            setValue={setCategory} 
+            list={formattedCategories} 
             style={styles.input}
-            right={<TextInput.Icon icon="chevron-down" />}
           />
 
           <Button 
