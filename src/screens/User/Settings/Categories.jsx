92af8c9a019642ccf71d4137975a9035aa21d94f
rfbittar <rafabittarg@gmail.com>
2024-11-26 18:23:56 -0300

    feat: organização de minhas categorias em ajustes

--- src/screens/User/Settings/Categories.jsx
+++ src/screens/User/Settings/Categories.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { List, FAB } from 'react-native-paper';
+import { List, FAB , Divider} from 'react-native-paper';
 import SafeScreen from '../../../components/SafeScreen';
+import { useFinances } from '../../../hooks/useFinances';
+
+
+
 
 const Categories = () => {
   const handleAddCategory = () => {
     // Implementar lógica para adicionar categoria
     console.log('Adicionar categoria');
   };
+  const handleEditCategory = () => {
+    console.log('Editar categoria');
+  }
+  const { categories } = useFinances();
+  console.log('categorias: ', { categories })
+
+  const expenseCategories = categories.filter(category => category.type === 'EXPENSE');
+  const incomeCategories = categories.filter(category => category.type === 'INCOME');
 
   return (
     <SafeScreen>
       <View style={styles.container}>
         <List.Section>
           <List.Subheader>Categorias de Despesas</List.Subheader>
-          <List.Item
-            title="Alimentação"
-            left={props => <List.Icon {...props} icon="food" />}
-          />
-          <List.Item
-            title="Transporte"
-            left={props => <List.Icon {...props} icon="car" />}
-          />
-          {/* Adicionar mais categorias aqui */}
+          {categories && expenseCategories.map((category, index) => (
+            <React.Fragment key={category.id}>
+              <List.Item
+                title={category.name}
+                left={props => <List.Icon {...props} icon="cart" color='red'/>}
+                onPress={() => handleEditCategory(category)}
+              />
+              {index < expenseCategories.length - 1 && <Divider />}
+            </React.Fragment>
+          ))}
+        </List.Section>
+        <List.Section>
+          <List.Subheader>Categorias de Receitas</List.Subheader>
+          {categories && incomeCategories.map((category, index) => (
+            <React.Fragment key={category.id}>
+              <List.Item
+                title={category.name}
+                left={props => <List.Icon {...props} icon="cash-multiple" color='green'/>}
+                onPress={() => handleEditCategory(category)}
+              />
+              {index < incomeCategories.length - 1 && <Divider />}
+            </React.Fragment>
+          ))}
         </List.Section>
       </View>
       <FAB
